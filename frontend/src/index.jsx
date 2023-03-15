5b8f55fbebcce458a82f5cae5c457517260d6191
EErikas <EErikas@users.noreply.github.com>
2023-03-15 22:05:47 +0200

    frontend uses geolocation and can show current weather and forecast

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 // Set Base URL based on the fact if it's running in development or production server
 const baseURL = ((import.meta.env.PROD) ? '/api' : 'http://localhost:9000/api');
 
-const getWeatherFromApi = async () => {
+const getWeatherFromApi = async (latitude, longitude) => {
   try {
-    const response = await fetch(`${baseURL}/weather`);
-    console.log(`Backend at ${baseURL}`);
+    const response = await fetch(`${baseURL}/weather/${latitude}/${longitude}`);
+    if(!import.meta.env.PROD){
+      console.log(`Backend at ${baseURL}`);
+    }
     return response.json();
   } catch (error) {
-    console.error(error);
+    if(!import.meta.env.PROD){
+      console.error(error);
+    }
+    return {};
   }
-
-  return {};
 };
 
 class Weather extends React.Component {
@@ -20,25 +24,67 @@ class Weather extends React.Component {
     super(props);
 
     this.state = {
-      icon: '',
+      city: '',
+      country: '',
+      todayIcon: '',
+      todayDescription: '',
+      tomorrowIcon: '',
+      tomorrowDescription: '',
     };
   }
 
   async componentDidMount() {
-    const weather = await getWeatherFromApi();
-    this.setState({ icon: weather.icon.slice(0, -1) });
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(async position => {
+        const { latitude, longitude } = position.coords;
+        const weather = await getWeatherFromApi(latitude, longitude);
+        this.setState({
+          city: weather.city,
+          country: weather.country,
+          geolocationEnabled: true,
+
+          todayIcon: weather.today.icon.slice(0, -1),
+          todayDescription: `${weather.today.main}: ${weather.today.description}`,
+          
+          tomorrowIcon: weather.tomorrow.icon.slice(0, -1),
+          tomorrowDescription: `${weather.tomorrow.main}: ${weather.tomorrow.description}`,
+        });
+      }, () => {
+        this.setState({ geolocationEnabled: false });
+      });
+    } else {
+      this.setState({ geolocationEnabled: false });
+    }
   }
 
   render() {
-    const { icon } = this.state;
-
+    const { todayIcon, todayDescription, tomorrowIcon, tomorrowDescription, city, country, geolocationEnabled } = this.state;
     return (
-      <div className="icon">
-        { icon && <img src={`/img/${icon}.svg`} alt="Weather" /> }
+      <div>
+        {!geolocationEnabled && <GeolocationPrompt />}
+
+        { city && country && <h1>Weather in {city}, {country}</h1> }
+        {todayIcon &&  todayDescription && <h2>Today: </h2>}
+        <div className="icon">
+          { todayIcon && <img src={`/img/${todayIcon}.svg`} alt={todayDescription} /> }
+        </div>
+
+        {tomorrowIcon && tomorrowDescription && <h2>Tomorrow: </h2>}
+        <div className="icon">
+          { tomorrowIcon && <img src={`/img/${tomorrowIcon}.svg`} alt={tomorrowDescription} /> }
+        </div>
       </div>
     );
   }
 }
+
+const GeolocationPrompt = () => {
+  return (
+    <div className="geolocation-prompt">
+      <p>Please enable geolocation in your browser to see the current weather for your location.</p>
+    </div>
+  );
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Weather />,
